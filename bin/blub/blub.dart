0053b88f864253ba395a7423f0d3eef78008a6c3
MetaMuffin <metamuffin@yandex.com>
2020-07-10 13:56:04 +0200

    a better but broken build system

--- bin/blub/blub.dart
+++ bin/blub/blub.dart
@@ -1,5 +1,4 @@
 import 'dart:io';
-
 import 'lib-blub/lib-blub.dart';
 
 class Program {
@@ -7,17 +6,94 @@ class Program {
   String data = '';
   ProgramContext pctx;
 
-  Program(
-      {void Function(ProgramContext c) entry,
-      void Function(ProgramContext c) data_entry}) {
+  String assemblerFile;
+  String relocatableFile;
+  String executableFile;
+
+  void Function(ProgramContext c) entry;
+
+  Program({
+    this.entry,
+    this.assemblerFile,
+    this.executableFile,
+    this.relocatableFile,
+  }) {
     pctx = ProgramContext(this);
-    entry(pctx);
+    if (!stdout.supportsAnsiEscapes) print('No ansi escape support. :(');
   }
 
-  void save(String filename) {
-    var f = File(filename);
+  String _success(String s) {
+    return '\033[32;1m${s}\033[0m';
+  }
+
+  String _info(String s) {
+    return '\033[32;1m${s}\033[0m';
+  }
+
+  void _print(String s) {
+    stdout.write(s);
+  }
+
+  void export() {
+    _print('Exporting...');
+    entry(pctx);
+    _print(' done\n');
+  }
+
+  void save() {
+    _print(_info('Saving assembler code to "$assemblerFile"'));
+    var f = File(assemblerFile);
     f.writeAsStringSync(
         'section data\n$data\nsection text\nglobal _start\n_start:\n$text');
+    _print('\n ${_success("done")}\n');
+  }
+
+  void compile({String format, List<String> args, String compiler}) async {
+    _print(_info('Compiling...'));
+
+    args ??= [];
+    compiler ??= 'nasm';
+    format ??= 'elf32';
+    var cargs = ['-f $format', '-o $relocatableFile'];
+    cargs.addAll(args);
+    cargs.add(assemblerFile);
+
+    _print(' \$ $compiler ${cargs.join(" ")}');
+    var results = await Process.run(compiler, cargs);
+    print(results.stderr);
+    _print(' ${_success("done")}\n');
+  }
+
+  void link(
+      {String linker,
+      List<String> args,
+      bool setExecutableAttrib = true}) async {
+    _print(_info('Linking...'));
+
+    args ??= [];
+    linker ??= 'ld';
+    var largs = ['-o', '${executableFile}'];
+    largs.addAll(args);
+    largs.add('$relocatableFile');
+
+    _print(' \$ $linker ${largs.join(" ")}');
+    var results = await Process.run(linker, largs);
+    print(results.stderr);
+    if (setExecutableAttrib) {
+      results = await Process.run('chmod', ['+x', executableFile]);
+    }
+    _print(' ${_success("done")}\n');
+  }
+
+  void run({List<String> args}) async {
+    _print(_info('Running...'));
+
+    args ??= [];
+    _print('\t \$ ./$executableFile ${args.join("")}\n');
+    var results = await Process.run('./$executableFile', args);
+    print(results.stdout);
+    print(results.stderr);
+    _print(' ${_success("done")}\n');
   }
 
   void writeCommand(String a) {
--- bin/hello.dart
+++ bin/hello.dart
@@ -1,13 +1,21 @@
 import 'blub/blub.dart';
 import 'blub/lib-blub/s-values.dart';
 
-void main() {
+void main() async {
   var con = Program(
+    assemblerFile: 'out.s',
+    relocatableFile: 'out.o',
+    executableFile: 'out',
     entry: (c) {
       c.assembler.COMMENT('Hello World Program');
       c.io.print_static_literal('Hello World!');
       c.system.exit(SLiteral(0));
     },
   );
-  con.save('out.s');
+
+  await con.export();
+  await con.save();
+  await con.compile(format: 'elf64');
+  await con.link();
+  await con.run();
 }
